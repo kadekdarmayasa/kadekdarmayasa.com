13bd07f8c84572195ce49d89f38c297018142d92
kadekdarmayasa <darmayasadiputra@gmail.com>
2023-10-24 13:49:29 +0800

    feat: finish footer section

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className='px-8 text-black mt-32 pb-10'>
+      <div className='sm:container px-20'>
+        <p className='text-xl leading-9 font-medium'>
+          &copy;2023 - Made with ReactJS and designed happily with Figma by
+          Kadek Darmayasa
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import Banner from './components/Banner'
 import HighlightedProject from './components/HighlightedProject'
 import Certification from './components/Certification'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -18,5 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Certification />
       <Contact />
     </main>
+    <Footer />
   </React.StrictMode>,
 )
