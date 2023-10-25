5cbca9865e5c1da78f8d2e4fe9934bdfcb4d9ca8
kadekdarmayasa <darmayasadiputra@gmail.com>
2023-10-25 10:18:17 +0800

    feat: add responsive feature to contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,19 +5,21 @@ import instagramLogo from '../assets/images/logo-instagram.svg'
 const Contact = () => {
   return (
     <section className='px-8 text-black mt-20'>
-      <div className='sm:container px-20'>
+      <div className='sm:container md:px-20'>
         <div>
-          <h3 className='text-3xl block font-bold'>🤝 Let's Get In Touch</h3>
-          <h2 className='text-5xl font-bold mt-10'>
+          <h3 className='text-2xl md:text-3xl block font-bold text-slate-500'>
+            🤝 Let's Get In Touch
+          </h3>
+          <h2 className='text-4xl md:text-5xl font-bold mt-10'>
             Do you have any project ideas or just want to say hi?
           </h2>
         </div>
 
-        <div className='flex mt-24 gap-10 items-center'>
+        <div className='flex mt-12 md:mt-24 gap-10 flex-col md:flex-row md:items-center'>
           <img
             src={myPictureX2}
             alt='Darmayasa at his college'
-            className='h-64'
+            className='h-64 w-64'
           />
           <div>
             <p className='text-xl leading-9 mb-8 font-medium'>Contact me via</p>
@@ -27,7 +29,7 @@ const Contact = () => {
                 className='flex items-center group mb-2'
                 rel='noopener noreferrer'
                 target='_blank'>
-                <div className='border-purple border-[1px] border w-10 h-10 rounded-md flex items-center justify-center group-hover:bg-purple/5 transition-all mr-4'>
+                <div className='border-purple border-[1px] w-10 h-10 rounded-md flex items-center justify-center group-hover:bg-purple/5 transition-all mr-4'>
                   <img src={emailLogo} alt='Darmayasa`s email address' />
                 </div>
                 <span className='text-xl leading-9 font-regular text-purple'>
@@ -39,7 +41,7 @@ const Contact = () => {
                 target='_blank'
                 rel='noopener noreferrer'
                 className='flex items-center group'>
-                <div className='border-purple border-[1px] border w-10 h-10 rounded-md flex items-center justify-center group-hover:bg-purple/5 transition-all mr-4'>
+                <div className='border-purple border-[1px] w-10 h-10 rounded-md flex items-center justify-center group-hover:bg-purple/5 transition-all mr-4'>
                   <img
                     src={instagramLogo}
                     alt='Darmayasa`s instagram account'
