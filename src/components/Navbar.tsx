20794d96bf05a8ef55840e4f456148639c74310b
kadekdarmayasa <darmayasadiputra@gmail.com>
2023-10-25 09:33:22 +0800

    feat: add responsive feature to the navbar section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,37 @@
 import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
+import menuIcon from '../assets/images/menu.svg'
 
 const menus = ['Home', 'Projects', 'Certifications', 'Contact Me']
 
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState<string>('Home')
+  const [isMenuListShown, setIsMenuListShown] = useState<boolean>(false)
 
   return (
-    <nav className='bg-light-gray h-auto px-8'>
-      <div className='sm:container px-20 h-16 flex items-center justify-between'>
+    <nav className='bg-light-gray/90 h-auto px-8 fixed top-0 left-0 right-0 z-40 backdrop-blur-md'>
+      <div className='md:container md:px-20 h-20 flex items-center justify-between'>
         <a href='/' className='text-black text-2xl font-extrabold'>
           DARMA<span className='text-purple'>.</span>
         </a>
-        <ul className='flex'>
+        <button
+          className='border-[1px] border-slate-900 h-8 w-8 flex justify-center items-center rounded-full hover:scale-105 transition-all md:hidden'
+          onClick={() => setIsMenuListShown((prevState) => !prevState)}>
+          <img src={menuIcon} alt='Click to toggle the menu list' />
+        </button>
+        <ul
+          className={`fixed md:static md:flex items-center bg-light-gray/90 backdrop-blur-md top-20 md:top-0 z-40 md:z-auto left-0 px-8 md:px-0 right-0 origin-top ${
+            isMenuListShown ? 'scale-y-100 opacity-1' : 'scale-y-0 opacity-0'
+          } transition-all md:scale-100 md:opacity-100`}>
           {menus.map((menu) => {
             return (
               <li
                 key={uuidv4()}
-                className='ml-5'
+                className='md:ml-5'
                 onClick={() => setActiveMenu(menu)}>
                 <a
                   href={`#${menu.toLowerCase()}`}
-                  className={`font-medium ${
+                  className={`font-medium text-lg hover:text-purple transition-all min-h-[44px] flex md:items-center md:justify-center ${
                     menu === activeMenu ? 'text-purple' : 'text-black'
                   }`}>
                   {menu}
